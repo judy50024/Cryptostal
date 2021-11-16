06a9fc846fefb5efe85c45b0ed85a4c685c60f48
Judy <judylee0428@gmail.com>
2021-11-16 19:36:48 +0800

    rwd

---
+++ js/all.js
@@ -0,0 +1,18 @@
+// hamburgur animation
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+// dropdown menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
